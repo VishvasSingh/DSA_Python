cf66c5caefdf86b1732ba759213eb3c1d6b32ba3
vishvassingh <vishvassingh.10816@gmail.com>
2024-08-01 13:30:13 +0530

    Linked List class, added methods, insert, set, get, pop_first

--- LinkedLists/linkedlist.py
+++ LinkedLists/linkedlist.py
@@ -83,5 +83,56 @@ class LinkedList:
 
         return True
 
+    def pop_first(self):
+        if not self.head:
+            return None
+
+        popped_node = self.head
+        self.head = self.head.next
+        popped_node.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+
+        return popped_node
+
+    def get(self, index: int):
+        if index < 0 or index >= self.length:
+            return None
+
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+
+        return temp
+
+    def set(self, index: int, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+
+        return False
+
+    def insert(self, index: int, value):
+        if index < 0 or index >= self.length:
+            return False
+
+        if index == 0:
+            return self.prepend(value)
+
+        if index == self.length:
+            return self.append(value)
+
+        new_node = Node(value)
+        temp = self.get(index)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+
+        return True
+
+
+
 
 
--- LinkedLists/test.py
+++ LinkedLists/test.py
@@ -3,12 +3,11 @@ from linkedlist import LinkedList
 
 link_list = LinkedList(4)
 
+for i in range(3):
+    link_list.append(i)
+
 link_list.print_list()
 
-print(link_list.pop())
-
-link_list.pop()
-
-link_list.append(8)
+link_list.set(2, 103)
 
 link_list.print_list()
